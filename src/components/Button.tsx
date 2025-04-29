feb1e05de1b5ef28a1d01ac36dced6f44303c07f
cfowlerdev <cfowlerdev@gmail.com>
2025-04-29 19:29:56 +0800

    first commit

---
+++ src/components/Button.tsx
@@ -0,0 +1,7 @@
+interface IButtonProps {
+  label: string;
+}
+
+export const Button = ({ label }: IButtonProps) => {
+  return <div>{label}</div>;
+};
---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), dts()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'MDEditor',
+      formats: ['es', 'umd'],
+      fileName: (format) => `md-editor.${format}.js`
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        }
+      }
+    },
+    sourcemap: true,
+    emptyOutDir: true
+  }
+});
